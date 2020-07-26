7c960c24ed62ecb95912dee4bc290c64f4aa51de
JoelKanyi <joelkanyi98@gmail.com>
2020-07-26 19:18:18 +0300

    adding some notes when the database is created: NoteDatabase

--- app/src/main/java/com/kanyideveloper/mvvmnoteapp/NoteDatabase.java
+++ app/src/main/java/com/kanyideveloper/mvvmnoteapp/NoteDatabase.java
@@ -1,10 +1,13 @@
 package com.kanyideveloper.mvvmnoteapp;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Note.class}, version = 1)
 public abstract class NoteDatabase extends RoomDatabase {
@@ -24,8 +27,34 @@ public abstract class NoteDatabase extends RoomDatabase {
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     NoteDatabase.class, "note_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallBack)
                     .build();
         }
         return  instance;
+    }
+
+    private static RoomDatabase.Callback roomCallBack  = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDBAsyncTask(instance).execute();
+        }
+    };
+
+
+    private static class PopulateDBAsyncTask extends AsyncTask<Void,Void,Void> {
+        private NoteDao noteDao;
+
+        private PopulateDBAsyncTask(NoteDatabase db){
+            noteDao = db.noteDao()
+        }
+
+        protected Void doInBackground(Void... voids) {
+            noteDao.insertNote(new Note("Title 1","Description 1", "Priority 1"));
+            noteDao.insertNote(new Note("Title 12","Description 12", "Priority 12"));
+            noteDao.insertNote(new Note("Title 13","Description 13", "Priority 13"));
+
+            return null;
+        }
     }
 }
