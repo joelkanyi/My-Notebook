c6c166cb4c63c6af29569493c67199af8b2e760b
JoelKanyi <joelkanyi98@gmail.com>
2020-07-26 20:24:10 +0300

    Initialised my adapter in the main activity

--- app/src/main/java/com/kanyideveloper/mvvmnoteapp/MainActivity.java
+++ app/src/main/java/com/kanyideveloper/mvvmnoteapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.kanyideveloper.mvvmnoteapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -18,11 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.allNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 //update our recyclerview
+                adapter.setNotes(notes);
                 Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
             }
         });
