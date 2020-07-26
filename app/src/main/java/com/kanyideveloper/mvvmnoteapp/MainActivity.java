ae450868c22599789056e26ed892c2a79a9a8325
JoelKanyi <joelkanyi98@gmail.com>
2020-07-26 19:40:37 +0300

    instance of the viewmodel in the main activity

--- app/src/main/java/com/kanyideveloper/mvvmnoteapp/MainActivity.java
+++ app/src/main/java/com/kanyideveloper/mvvmnoteapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.kanyideveloper.mvvmnoteapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.allNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                //update our recyclerview
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
